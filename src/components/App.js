c7f65065f4c559b8753a073f7e6221e3a50ae08c
Michael Trew <mtrew2015@gmail.com>
2022-03-03 16:31:52 -0500

    making game playable again

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,23 @@ import React, {Component} from 'react';
 import Game from './Game.js';
 
 export default class App extends Component {
+  state = {
+    gameId: 1,
+  };
+
+  resetGame = () => {
+    this.setState(prevState => {
+      return {gameId: prevState.gameId + 1};
+    });
+  };
   render() {
-    return <Game randomNumberCount={6} />;
+    return (
+      <Game
+        key={this.state.gameId}
+        randomNumberCount={6}
+        initialSeconds={10}
+        onPlayAgain={this.resetGame}
+      />
+    );
   }
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,23 +1,62 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import PropTypes from 'prop-types';
 import RandomNumber from './RandomNumber';
+import {shuffle} from 'lodash';
 
 export default class Game extends Component {
   static propTypes = {
     randomNumberCount: PropTypes.number.isRequired,
+    initialSeconds: PropTypes.number.isRequired,
+    onPlayAgain: PropTypes.func.isRequired,
   };
 
   state = {
     selectedIds: [],
+    remainingSeconds: this.props.initialSeconds,
   };
   randomNumbers = Array.from({length: this.props.randomNumberCount}).map(
     () => 1 + Math.floor(10 * Math.random()),
   );
 
+  gameStatus = 'PLAYING';
+
   target = this.randomNumbers
     .slice(0, this.props.randomNumberCount - 2)
     .reduce((acc, curr) => acc + curr, 0);
+
+  shuffledRandomNumbers = shuffle(this.randomNumbers);
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(
+        prevState => {
+          return {remainingSeconds: prevState.remainingSeconds - 1};
+        },
+        () => {
+          if (this.state.remainingSeconds === 0) {
+            clearInterval(this.intervalId);
+          }
+        },
+      );
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
+    if (
+      nextState.selectedIds !== this.state.selectedIds ||
+      nextState.remainingSeconds === 0
+    ) {
+      this.gameStatus = this.calcGameStatus(nextState);
+      if (this.gameStatus !== 'PLAYING') {
+        clearInterval(this.intervalId);
+      }
+    }
+  }
 
   isNumberSelected = numberIndex => {
     return this.state.selectedIds.indexOf(numberIndex) >= 0;
@@ -28,10 +67,13 @@ export default class Game extends Component {
       selectedIds: [...prevState.selectedIds, numberIndex],
     }));
   };
-  gameStatus = () => {
-    const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
-      return acc + this.randomNumbers[curr];
+  calcGameStatus = nextState => {
+    const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+      return acc + this.shuffledRandomNumbers[curr];
     }, 0);
+    if (nextState.remainingSeconds === 0) {
+      return 'LOST';
+    }
     if (sumSelected < this.target) {
       return 'PLAYING';
     }
@@ -45,14 +87,14 @@ export default class Game extends Component {
   };
 
   render() {
-    const gameStatus = this.gameStatus();
+    const gameStatus = this.gameStatus;
     return (
       <View style={styles.container}>
         <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
           {this.target}
         </Text>
         <View style={styles.randomContainer}>
-          {this.randomNumbers.map((number, index) => (
+          {this.shuffledRandomNumbers.map((number, index) => (
             <RandomNumber
               isDisabled={
                 this.isNumberSelected(index) || gameStatus !== 'PLAYING'
@@ -64,6 +106,9 @@ export default class Game extends Component {
             />
           ))}
         </View>
+        {this.gameStatus !== 'PLAYING' && (
+          <Button title="Play Again" onPress={this.props.onPlayAgain} />
+        )}
       </View>
     );
   }
